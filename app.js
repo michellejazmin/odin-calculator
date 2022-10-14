6159de7ac2c2eabeab9860ba04279f872909ff31
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-14 19:55:09 -0300

    Add basic functionaly

---
+++ app.js
@@ -0,0 +1,63 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "You can't divide by 0!";
+  return a / b;
+}
+
+/* function operate(a, operator, b) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+  
+    case "-":
+      result = subtract(a, b);
+      break;
+
+    case "*":
+      result = multiply(a, b);
+      break;
+    
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+} */
+
+const displayValue = document.getElementById('display-value');
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => displayValue.textContent = '0');
+
+const backspace = document.getElementById('backspace');
+backspace.addEventListener('click', () displayValue.textContent = displayValue.textContent)
+
+const numberKeys = document.getElementsByClassName('number-key');
+const operatorKeys = document.getElementsByClassName('operator-key');
+
+function populateDisplay(str) {
+  if (displayValue.textContent === '0') displayValue.textContent = str;
+  else displayValue.textContent += str;
+}
+
+for (key of numberKeys) {
+  key.addEventListener('click', (e) => populateDisplay(e.srcElement.id));
+}
+
+for (key of operatorKeys) {
+  key.addEventListener('click', (e) => populateDisplay(e.srcElement.textContent));
+}
+
+populateDisplay('0');
