3162c667f22927ed5e3305fe815fdc14f25a942a
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-15 00:52:09 -0300

    Add functionality to operator and number buttons

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return "You can't divide by 0!";
-  return a / b;
+  if (b === 0) {
+    alert("You cannot divide by 0!");
+    return 0;
+  }
+  return (a / b);
 }
 
-/* function operate(a, operator, b) {
-  let result;
+function operate(num1, num2, sign) {
+  let result = 0;
+  let a = Number(num1);
+  let b = Number(num2);
 
-  switch (operator) {
+  switch (sign) {
     case "+":
       result = add(a, b);
       break;
@@ -27,26 +32,36 @@ function divide(a, b) {
       result = subtract(a, b);
       break;
 
-    case "*":
+    case "×":
       result = multiply(a, b);
       break;
     
-    case "/":
+    case "÷":
       result = divide(a, b);
       break;
-  }
 
-  return result;
-} */
+    default:
+      alert("That's not a valid operation");
+  }
+    
+  if (!Number.isInteger(result)) {
+    result = result.toFixed(3);
+  }
+  
+  displayValue.textContent = result;
+  input.splice(0);
+  operator = '';
+}
+
 
 const displayValue = document.getElementById('display-value');
 const input = [];
+let operator = '';
+
 
 function populateDisplay(str) {
   if (displayValue.textContent === '0') displayValue.textContent = str;
   else displayValue.textContent += str;
-
-  input.push(str);
 }
 
 function deleteFromDisplay() {
@@ -60,7 +75,9 @@ function deleteFromDisplay() {
 function clearDisplay() {
   displayValue.textContent = '0'
   input.splice(0, input.length);
+  operator = '';
 }
+
 
 const clear = document.getElementById('clear');
 clear.addEventListener('click', () => clearDisplay());
@@ -69,20 +86,44 @@ const backspace = document.getElementById('backspace');
 backspace.addEventListener('click', () => deleteFromDisplay());
 
 const numberKeys = document.getElementsByClassName('number-key');
-const operatorKeys = document.getElementsByClassName('operator-key');
-
-
 for (key of numberKeys) {
-  key.addEventListener('click', (e) => populateDisplay(e.srcElement.id));
+  key.addEventListener('click', (e) => populateDisplay(e.target.id));
 }
 
+// TODO: point button functionality
+const pointKey = document.querySelector('.point-key');
+pointKey.addEventListener('click', (e) => populateDisplay(e.target.textContent));
+
+const operatorKeys = document.getElementsByClassName('operator-key');
 for (key of operatorKeys) {
-  key.addEventListener('click', (e) => populateDisplay(e.srcElement.textContent));
+  key.addEventListener('click', (e) => {
+    if (input.length === 0 && displayValue.textContent === '0') {
+      if (e.target.id === 'minus-key') {
+        populateDisplay(e.target.textContent);
+      } else return;
+    } else if (input.length === 0 && displayValue.textContent !== '0') {
+      input[0] = displayValue.textContent;
+      operator = e.target.textContent;
+      populateDisplay(e.target.textContent);
+    } else if (input.length === 1) {
+        if (operator) {
+          deleteFromDisplay();
+          operator = e.target.textContent;
+          populateDisplay(e.target.textContent);
+        } else {
+          operator = e.target.textContent;
+        }
+    } else operate(...input, operator);    
+  });
 }
 
 const equalsKey = document.querySelector('.equals-key');
-equalsKey.addEventListener('click', () => operate(...input))
+equalsKey.addEventListener('click', () => {
+  if (input.length === 0 || operator === '') return;
+  let operatorIndex = displayValue.textContent.indexOf(operator);
+  input[1] = displayValue.textContent.slice(operatorIndex + 1);
+  operate(...input, operator);
+});
 
-const pointKey = document.querySelector('.point-key');
 
 populateDisplay('0');
